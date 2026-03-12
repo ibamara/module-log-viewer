d03e30b3a64d9a2ef48c9999dd384c583c2cebd0
ibamara <38118242+ibamara@users.noreply.github.com>
2026-03-12 21:38:24 +0000

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: ibamara
+ * Date: 29.09.2025
+ * Time: 14:05
+ */
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ibamara_LogViewer',
+    __DIR__
+);
+
